769585bab8095d7d8ae22235a3bfaabd275e735d
Chris Knowles <chris.knowles@thinkmoney.co.uk>
2023-09-27 15:24:09 +0100

    button added and code refactored to work with any size grid and a new grid by the click of a button

--- index.js
+++ index.js
@@ -1,40 +1,60 @@
 console.clear();
 
-        
+// add a button at the top asking new grid
+// addEventlistner to button that prompts the user to enter a number
+// validate the number is between 2 and 100 
+// the number should thna create a new grid
+
+const container = document.getElementById("container");
+const button = document.createElement("button");
+button.innerText = "New Grid ?";
+
+container.insertAdjacentElement('beforebegin', button);
+
+button.addEventListener('click', function () {
+    const userInputValue = userInput();
+    if (userInputValue !== null) {
+        createGrid(userInputValue); // Call createGrid with the user input
+    }
+});
+
+function userInput() {
+    let userInput = prompt("Please enter the size of the grid 2 to 100");
+    if (userInput < 2 || userInput > 100 || userInput === '') {
+        userInput();
+    } else {
+        return userInput;
+    }
+}
+
+
 function createGrid(number) {
-    const container = document.getElementById("container");
+    container.innerHTML = '';
+    
+        for (let rows = 0; rows < number; rows++) {
+            for (let columns = 0; columns < number; columns++) {
+                const div = document.createElement("div");
+                div.className = "grid";
+                container.appendChild(div);
+            }
+        }
         
-    for (let rows = 0; rows < number; rows++) {
-        for (let columns = 0; columns < number; columns++) {
-            const div = document.createElement("div");
-            div.className = "grid";
-            container.appendChild(div);
+        const gridElements = document.querySelectorAll(".grid");
+        gridElements.forEach((element) => {
+            element.style.width = 800 / number + "px";
+            element.style.height = 800 / number + "px";
+        });
+        const divs = document.getElementsByClassName('grid');
+        
+        for (let i = 0; i < divs.length; i++) {
+            divs[i].addEventListener('mouseover', function (specificDiv) {
+                specificDiv.target.style.backgroundColor = 'red';
+        
+            });
         }
     }
-        
-    const gridElements = document.querySelectorAll(".grid");
-    gridElements.forEach((element) => {
-        element.style.width = 800 / number + "px";
-        element.style.height = 800 / number + "px";
-    });
-}
-        
-createGrid(30);
-
-//hover states
-// 💭 css .grid: hover {} will change the grid when the mouse is hovering over it
-// 💭 can i use event listener to see if the mouse has hovered adn change the state of that particular div?
-// what am i listening for - mouse over
-// mouse over what - the specific div in the grid
-    // will need to listen to all gird squares - need to loop the eventlistener through all the divs (think RPS loop over buttons)
-// what to do - change background to red
-
-
-const divs = document.getElementsByClassName('grid');
-
-for (let i = 0; i < divs.length; i++) {
-    divs[i].addEventListener('mouseover', function (specificDiv) {
-        specificDiv.target.style.backgroundColor = 'red';
-
-    });
-}
+    
+    createGrid(userInput());
+    
+    
+    
