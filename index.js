a04fd1445aedf167ce677e8977fafc02808fefb1
Chris Knowles <chris.knowles@thinkmoney.co.uk>
2023-09-27 16:22:26 +0100

    refined the user input conditions

--- index.js
+++ index.js
@@ -2,22 +2,20 @@ console.clear();
 
 const container = document.getElementById("container");
 const button = document.createElement("button");
-button.innerText = "New Grid ?";
-
-container.insertAdjacentElement('beforebegin', button);
+    button.innerText = "New Grid ?";
+    container.insertAdjacentElement('beforebegin', button);
 
 button.addEventListener('click', function () {
     const userInputValue = userInput();
-    if (userInputValue !== null) {
-        createGrid(userInputValue); // Call createGrid with the user input
-    }
+     createGrid(userInputValue); // Call createGrid with the user input
+    
 });
 
 function userInput() {
     let userInput;
     do {
         userInput = prompt("Please enter the size of the grid (2 to 100)");
-    } while (userInput === null || userInput === '' || userInput < 2 || userInput > 100);
+    } while (userInput === null || userInput < 2 || userInput > 100);
 
     return userInput;
 }
@@ -40,8 +38,9 @@ function createGrid(number) {
             element.style.height = 800 / number + "px";
         });
     
-        const divs = document.getElementsByClassName('grid');
         
+    const divs = document.getElementsByClassName('grid');
+    
         for (let i = 0; i < divs.length; i++) {
             divs[i].addEventListener('mouseover', function (specificDiv) {
                 specificDiv.target.style.backgroundColor = 'red';
